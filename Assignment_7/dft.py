17885ec5240e2f560b3dc18ee793c27dd2d04116
Nick <nv314961@bu.edu>
2017-10-31 16:09:13 -0400

    Progress in-class
    todo: implement ValueError checks

--- Assignment_7/dft.py
+++ Assignment_7/dft.py
@@ -1,3 +1,28 @@
-from numpy import zeros, exp, array, pi
+from numpy import zeros, exp, array, pi #, fft, set_printoptions, allclose
 
+# x: valid sequence of numbers
+# N: size of x
 def DFT(x):
+	# ensure input is a valid numerical sequence
+	# raise ValueError
+	
+	N = x.size
+	X = zeros((N,), dtype=complex)
+	for k in range(N):
+		for n in range(N):
+			X[k] += x[n]*exp(-1j * 2*pi * k * n / N)
+
+	#set_printoptions(suppress=True)
+	#print(X)
+	#Y = fft.fft(x)
+	#print(Y)
+	#print(allclose(X,Y))
+
+	return X
+
+def main():
+	x = array([1,2,3,4,"B",6,7,8])
+	DFT(x)
+	
+if __name__ == '__main__':
+    main()
